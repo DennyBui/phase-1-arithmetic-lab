e9e1f43edcaac0695ec63306754e9bb6548e4d16
DennyBui <dbuidmd2018@gmail.com>
2023-03-16 22:19:27 -0700

    arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let random = Math.ceil(Math.random() * 10 + 1); //generates a random number between 1 and 10
+let num3 = 20;
+let num4 = 8;
+let set = [5, 10, 15, 20]
+let max = Math.max(5, 10, 15, 20)
+const multiply = num1 * num2
+const mod = num3 % num4
+multiply(num1, num2) 
+function random() {
+    return Math.floor(Math.random() * 10) + 1;
+};
+mod(num3, num4) 
+function max(set) {
+    return Math.max(5, 10, 15, 20)
+};
